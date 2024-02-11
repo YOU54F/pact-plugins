c98066e5fdb62938d8685ad78b4f58d66909264f
Yousaf Nabi <you@saf.dev>
2024-02-11 14:07:02 +0000

    feat: musl variants of csv plugin

--- plugins/csv/release.sh
+++ plugins/csv/release.sh
@@ -17,10 +17,26 @@ case "$1" in
             gzip -c target/release/pact-csv-plugin > release_artifacts/pact-csv-plugin-linux-x86_64.gz
             openssl dgst -sha256 -r release_artifacts/pact-csv-plugin-linux-x86_64.gz > release_artifacts/pact-csv-plugin-linux-x86_64.gz.sha256
             cp pact-plugin.json release_artifacts/
-            cargo install cross
+            cargo install cross@0.2.5
+
+            echo -- Build the aarch64 release artifacts --
+            cargo clean
             cross build --target aarch64-unknown-linux-gnu --release
             gzip -c target/aarch64-unknown-linux-gnu/release/pact-csv-plugin > release_artifacts/pact-csv-plugin-linux-aarch64.gz
             openssl dgst -sha256 -r release_artifacts/pact-csv-plugin-linux-aarch64.gz > release_artifacts/pact-csv-plugin-linux-aarch64.gz.sha256
+
+            echo -- Build the musl x86_64 release artifacts --
+            cargo clean
+            cross build --release --target=x86_64-unknown-linux-musl
+            gzip -c target/x86_64-unknown-linux-musl/release/pact-csv-plugin > release_artifacts/pact-csv-plugin-linux-x86_64-musl.gz
+            openssl dgst -sha256 -r release_artifacts/pact-csv-plugin-linux-x86_64-musl.gz > release_artifacts/pact-csv-plugin-linux-x86_64-musl.gz.sha256
+
+            echo -- Build the musl aarch64 release artifacts --
+            cargo clean
+            cross build --release --target=aarch64-unknown-linux-musl
+            gzip -c target/aarch64-unknown-linux-musl/release/pact-csv-plugin > release_artifacts/pact-csv-plugin-linux-aarch64-musl.gz
+            openssl dgst -sha256 -r release_artifacts/pact-csv-plugin-linux-aarch64-musl.gz > release_artifacts/pact-csv-plugin-linux-aarch64-musl.gz.sha256
+
             ;;
   Windows)  echo  "Building for Windows"
             cargo build --release
